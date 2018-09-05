3ad9a94e66512ebd7c5d83fd66708ed0c47c2607
mjnrock <mjnrock@umich.edu>
2018-09-05 17:31:28 -0400

    init

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-	echo "Hello";
-?>
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css" />
+
+		<title>Storyline</title>
+	</head>
+	<body>
+		
+	</body>
+</html>
